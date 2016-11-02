009cc72728cdd15f196e93b236f8299819c02872
sv8743 <vignesh.sm@zoho.com>
2016-11-03 00:01:02 +0530

    Removed unused imports

--- app/src/main/java/com/vicky/notifier/database/DatabaseHelper.java
+++ app/src/main/java/com/vicky/notifier/database/DatabaseHelper.java
@@ -1,14 +1,5 @@
 package com.vicky.notifier.database;
 
-import static com.vicky.notifier.database.DatabaseConstants.EVENT_ID;
-import static com.vicky.notifier.database.DatabaseConstants.EVENTS_TABLE_NAME;
-import static com.vicky.notifier.database.DatabaseConstants.DATABASE_NAME;
-import static com.vicky.notifier.database.DatabaseConstants.EVENT_DAY;
-import static com.vicky.notifier.database.DatabaseConstants.EVENT_MONTH;
-import static com.vicky.notifier.database.DatabaseConstants.EVENT_NAME;
-import static com.vicky.notifier.database.DatabaseConstants.MONTH_NAMES;
-import static com.vicky.notifier.database.DatabaseConstants.EQUALS;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -17,11 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.vicky.notifier.database.model.DatabaseInterface;
 import com.vicky.notifier.database.util.QueryUtil;
-import com.vicky.notifier.view.BulkEventDetails;
-import com.vicky.notifier.view.EventDetails;
 
 import java.util.List;
-import java.util.Map;
 
 public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseInterface {
 
